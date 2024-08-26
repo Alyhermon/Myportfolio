6474f2e9b7146dde171a96485683665276a6e68f
alyhermon <Alinahermon11@gmail.com>
2024-08-26 13:25:05 -0400

    Name

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,6 +7,7 @@ export default function RootLayout({ children }) {
         <title> Portafolio </title>
       </head>
       <body>
+        <h1>Alina Marina Hermon Castro</h1>
         {children}
         </body>
     </html>
