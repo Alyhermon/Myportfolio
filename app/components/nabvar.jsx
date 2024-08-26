e814fa823875784dd2e1134d36d51b1b2138bb00
alyhermon <Alinahermon11@gmail.com>
2024-08-26 14:58:48 -0400

    changes

--- app/components/nabvar.jsx
+++ app/components/nabvar.jsx
@@ -1,5 +1,28 @@
 import Link from 'next/link'
 
 export default function Nabvar() {
-    return 
+    return (
+      <div>
+        <nav>
+          <h1>Alina M. Hermon Castro</h1>
+        </nav>
+        <ul>
+          <li>
+              <Link href="/">Inicio</Link>
+          </li>
+          <li>
+              <Link href="/proyectos">Proyectos</Link>
+          </li>
+          <li>
+              <Link href="/aboutme">Sobre mi</Link>
+          </li>
+          <li>
+              <Link href="/experiencia">Experiencia</Link>
+          </li>
+          <li>
+              <Link href="/contactos">Contacto</Link>
+          </li>
+        </ul>
+      </div>
+    );
   }
