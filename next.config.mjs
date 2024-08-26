4f6a6297568c8be674880ab501604dc5b94ee260
alyhermon <Alinahermon11@gmail.com>
2024-08-26 05:34:29 -0400

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
+const nextConfig = {
+    // Configuración para la exportación estática
+    trailingSlash: true, // Asegura que las URLs terminen en '/'
+    images: {
+      unoptimized: true, // Desactiva la optimización de imágenes, ya que GitHub Pages no soporta la optimización de imágenes de Next.js
+    },
+    // Puedes agregar otras configuraciones aquí si es necesario
+  };
 export default nextConfig;
